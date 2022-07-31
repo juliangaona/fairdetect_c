be3cae53bfb656f87d5e579d28e6574dfdc26f9d
Julián Gaona <108943905+juliangaona@users.noreply.github.com>
2022-07-31 18:03:19 -0500

    Add files via upload

---
+++ extended_dataframe_group_c/groupc_extended_df/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='extendeddataframeteamc',
+      version='0.2',  # Development release
+      description='Extends the pandas dataframe with new functionalities. ',
+      url='https://github.com/juliangaona/fairdetect_c',
+      author='Team_C_GMBD_2022_IE_UNIVERSITY',
+      author_email='julian.gaona.gonzalez@gmail.com',
+      license='GNU General Public License v3.0 ',
+          packages=['groupc_extended_df'],
+      zip_safe=False)
