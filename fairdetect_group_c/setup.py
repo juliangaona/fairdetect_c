a0adc06e1be44855a3fa5820aaab2c61eb51270d
Julián Gaona <108943905+juliangaona@users.noreply.github.com>
2022-07-31 18:01:34 -0500

    Add files via upload

---
+++ fairdetect_group_c/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fairdetectteamc',
+      version='0.1',  # Development release
+      description='FairDetect class to detect bias, demonstrated by Team C. There are improvements included along with docstrings. ',
+      url='https://github.com/juliangaona/fairdetect_c',
+      author='Team_C_GMBD_2022_IE_UNIVERSITY',
+      author_email='julian.gaona.gonzalez@gmail.com',
+      license='GNU General Public License v3.0 ',
+          packages=['fairdetect_group_c'],
+      zip_safe=False)
